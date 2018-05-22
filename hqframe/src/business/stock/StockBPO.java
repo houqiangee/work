a481ee1ac4c11686f7f199f0e07dda3cc9fa2be6
houqiangee <604576142@qq.com>
2018-05-22 20:10:06 +0800

    1

--- hqframe/src/business/stock/StockBPO.java
+++ hqframe/src/business/stock/StockBPO.java
@@ -1,8 +1,42 @@
 package business.stock;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.framework.layer.BPO;
 
 
 public class StockBPO extends BPO{
 	
+	public static void main(String[] str) throws Exception{
+		System.out.println("ks");
+		InputStream input = new FileInputStream(new File("H:/股票列表h.txt"));
+		ByteArrayOutputStream swapStream = new ByteArrayOutputStream(); 
+		byte[] buff = new byte[100]; //buff用于存放循环读取的临时数据 
+		int rc = 0; 
+		while ((rc = input.read(buff, 0, 100)) > 0) { 
+			swapStream.write(buff, 0, rc); 
+		} 
+		byte[] in_b = swapStream.toByteArray();
+		String stri=new String(in_b);
+		String regEx ="<a.*?>(.*?)</a>?";
+		
+		Pattern p = Pattern.compile(regEx);
+        Matcher m = p.matcher(stri);
+        while(m.find()){
+        	String d=m.group();
+        	d=d.replaceAll("<.*?>", "");
+        	Pattern p1 = Pattern.compile("\\(.*\\)");
+            Matcher m1 = p1.matcher(d);
+            m1.find();
+            String gpdm=m1.group().replace("(", "").replace(")", "");
+            String gpmc=d.replaceAll("\\(.*?\\)", "");
+            System.out.println(gpdm);
+        	System.out.println(gpmc);
+        }
+	}
 }
